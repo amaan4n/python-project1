2936dc1e1eec108d88f40b4f0b677da88a2dfbe5
Syed Amaan Mehmoodi <146960899+amaan4n@users.noreply.github.com>
2023-11-13 14:31:58 +0530

    Add files via upload

---
+++ proj1.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Open a connection to the webcam (usually 0 or 1 depending on your system)
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    # Break the loop if 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the camera and close the window
+cap.release()
+cv2.destroyAllWindows()
